3d452d07468b2e0f51d9f273ed6f7c1ebb8dc1f2
xupangpan <841292619@qq.com>
2020-07-10 12:25:54 +0800

    对项目实现一些优化‘

--- src/register.java
+++ src/register.java
@@ -1,15 +1,14 @@
-import...
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+
 
 
 @WebServlet(name = "registerServlet", urlPatterns = "/register.htm")
